5c360edb0a6f6b80f7867aac7a13fafbbfce8ba5
ayse <atrk61@gmail.com>
2021-06-07 23:55:16 +0200

    Mtd Bank icin first commit

---
+++ mtdbank/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from "./home/Home";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+function App() {
+  return (
+  <Router>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component = {Home} />
+      </Switch>
+      <Footer />
+    </div>
+  </Router>
+  ); 
+}
+export default App;
---
+++ mtdbank/src/home/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./Home.css"; 
+const home = () => {
+    return (
+        <div className="home">Home comes here</div>
+    )
+}
+export default home
